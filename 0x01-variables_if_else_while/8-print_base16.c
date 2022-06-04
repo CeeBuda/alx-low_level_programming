180b3d3970c6466d1597c91d631730f1589eb302
CeeBuda <sinnocent506@gmail.com>
2022-06-03 20:32:37 -0700

    Hexadecimal using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char chr;
+
+	for (num = 0; num < 10; num++)
+	putchar(chr);
+
+	putchar('\n');
+	return (0);
+}
