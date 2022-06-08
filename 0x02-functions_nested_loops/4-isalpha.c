8fb6b5897d37ba61085a956d6af69f555f25f5cd
CeeBuda <sinnocent506@gmail.com>
2022-06-07 18:29:44 -0700

    Checking for lower and uppercase alaphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_isalpha - checks for alphabetic character
+ *
+ *@c: Input value of type int
+ *
+ *Return: returns 1 if c is lower or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
