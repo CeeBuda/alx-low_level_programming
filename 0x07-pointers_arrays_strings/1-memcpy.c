67871669607d25c0cc5ae228388c353857eaea86
CeeBuda <sinnocent506@gmail.com>
2022-06-22 00:16:09 +0000

    Understanding how memcpy works internally

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination where memory area is copied to
+ * @src: source where memory area is copied from
+ * @n: size to be copied
+ *
+ * Return: returns a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest [i] = src [i];
+	}
+	return (dest);
+}		
