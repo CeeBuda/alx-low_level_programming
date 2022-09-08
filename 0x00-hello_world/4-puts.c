2bfa57ce4262f7402671b060cfa73b000de44027
CeeBuda <sinnocent506@gmail.com>
2022-09-08 15:20:35 +0000

    Printing a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
-	char str [] = "\"Programming is like building a multilingual puzzle";
-	
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 
 	return (0);
