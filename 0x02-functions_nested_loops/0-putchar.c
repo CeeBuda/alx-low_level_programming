7cae528131218f5735a2f572ce1c26601734d259
CeeBuda <sinnocent506@gmail.com>
2022-09-13 14:06:28 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar to stdout
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	
+	printf("%s\n", str);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is about C funtions and using nested loops
