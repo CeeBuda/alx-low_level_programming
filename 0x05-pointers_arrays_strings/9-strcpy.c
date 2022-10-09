93c446855433a39715b9bd7bd0f77f2cf865a6c5
CeeBuda <sinnocent506@gmail.com>
2022-10-09 18:16:09 +0000

    copying a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strcpy - copies a string 
+ * _strcpy - copies a string
  * @src: pointer to the string to be copied
  * @dest: buffer where string will be copied into
  *
- * Return: pointer to dest 
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
