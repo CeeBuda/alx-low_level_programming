786729ba30fa0f69fa3bea1966eb348031529a19
CeeBuda <sinnocent506@gmail.com>
2022-10-10 11:25:54 +0000

    Creating a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - strucure for capturing dog details
+ * @name: name of the dog
+ * @age: how old is the dog
+ * @owner: who owns the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
