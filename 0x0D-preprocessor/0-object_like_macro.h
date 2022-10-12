fb095f8672914e0e06350f67bb281551976dbc6e
CeeBuda <sinnocent506@gmail.com>
2022-10-12 00:00:26 +0000

    defining a macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef 0_-_OBJECT__LIKE__MACRO_H
-#define 0_-_OBJECT__LIKE__MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 #define SIZE 1024
 
-#endif /* 0_-_OBJECT__LIKE__MACRO_H */ 
+#endif /* OBJECT_LIKE_MACRO_H */ 
