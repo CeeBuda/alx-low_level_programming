ed0d9871581a952521ce50adce28621f274d7ee6
CeeBuda <sinnocent506@gmail.com>
2022-10-13 18:09:45 +0000

    printing strings passed as parameters in a function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char*);
-		
+
 		if (str == NULL)
 		{
 			printf("(nil)");
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 		}
-		
+
 		printf("%s", str);
 
 		if (i < (n - 1))
